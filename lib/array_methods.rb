52ca05f74625cf0ab8bc0081ceaf14f9662173ac
Oscar <oscar_ochoac@yahoo.com>
2019-06-29 18:11:01 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,7 +8,7 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  max = NIL
+  max = 0
   array.length.times { |index|
   if array[index] > max
     max = array[index]
@@ -17,10 +17,10 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  min = NIL
+  min = 
   array.length.times { |index|
-  if array[index] < max
-    max = array[index]
+  if array[index] < min
+    min = array[index]
   end
   }
 end
